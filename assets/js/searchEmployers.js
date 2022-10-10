f4b065ebaa05ece998a42b165df326627d38097c
hiba khlifi <hiba.khlifi12@gmail.com>
2022-10-10 22:23:14 +0100

    make response filter nav and fixed the toggle to show list items

--- assets/js/searchEmployers.js
+++ assets/js/searchEmployers.js
@@ -1,69 +1,54 @@
 //variable declaration
-const selectBtn = document.querySelector(".select-btn"),
-      items = document.querySelectorAll(".item");
-      emploie=document.querySelector("#emploie"),
-      teletravail=document.querySelector("#teletravail"),
-      annualsalary=document.querySelector("#annual-salary"),
-      compte=document.querySelector("#compte"),
-      dayrate=document.querySelector("#dayrate"),
+const selectBtn = document.querySelectorAll(".select-btn");
+items = document.querySelectorAll(".item");
+
 //********nav filter************** */
-selectBtn.addEventListener("click", () => {
-    selectBtn.classList.toggle("open");
-});
-dayrate.addEventListener("click", () => {
-    dayrate.classList.toggle("open");
-});
-
-emploie.addEventListener("click", () => {
-    emploie.classList.toggle("open");
-});
-teletravail.addEventListener("click", () => {
-    teletravail.classList.toggle("open");
-   
-    
-});
-annualsalary.addEventListener("click", () => {
-    annualsalary.classList.toggle("open");
-});
-compte.addEventListener("click", () => {
-    compte.classList.toggle("open");
-});
-
-items.forEach(item => {
-    item.addEventListener("click", () => {
-        item.classList.toggle("checked");
-
-        let checked = document.querySelectorAll(".checked"),
-            btnText = document.querySelector(".btn-text");
-
-            // if(checked && checked.length > 0 && selectBtn){
-            //     btnText.innerText = `${checked.length} Selected`;
-            // }else{
-            //     btnText.innerText = "Select categories";
-            // }
-    });
-})
-
-
-
-      
-
-$(document).ready(function(){
-    $('.menu-btn').click(function(){
-        $('.simple-sidebar').addClass('active')
-         $('.menu-btn').addClass('disable')
-         $('.main-section').removeClass('main-section col-lg-11 col-md-12 col-sm-12')
-         $('.main-section').addClass('col-lg-8')
-      
+const closeOtherToggleButton= (item)=>{
+    return selectBtn.forEach(el=>{
+        if(el != item){
+            el.classList.remove('open')
+        }
     })
-    $('.close-btn').click(function(){
-        $('.simple-sidebar').removeClass('active')
-        $('.menu-btn').removeClass('disable')
-    })
-})
+}
 
 
-console.log(window.innerWidth)
+selectBtn.forEach((item) =>
+  item.addEventListener("click", () => {
+    item.classList.toggle("open");
+    closeOtherToggleButton(item)
+
+  })
+);
 
 
+items.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("checked");
 
+    let checked = document.querySelectorAll(".checked"),
+      btnText = document.querySelector(".btn-text");
+
+    // if(checked && checked.length > 0 && selectBtn){
+    //     btnText.innerText = `${checked.length} Selected`;
+    // }else{
+    //     btnText.innerText = "Select categories";
+    // }
+  });
+});
+
+$(document).ready(function () {
+  $(".menu-btn").click(function () {
+    $(".simple-sidebar").addClass("active");
+    $(".menu-btn").addClass("disable");
+    $(".main-section").removeClass(
+      "main-section col-lg-11 col-md-12 col-sm-12"
+    );
+    $(".main-section").addClass("col-lg-8");
+  });
+  $(".close-btn").click(function () {
+    $(".simple-sidebar").removeClass("active");
+    $(".menu-btn").removeClass("disable");
+  });
+});
+
+console.log(window.innerWidth);
