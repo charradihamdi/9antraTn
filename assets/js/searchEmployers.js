f1d6bc4c6b849825d0252f9c3f3ca73f5478ab00
hiba khlifi <hiba.khlifi12@gmail.com>
2022-10-12 20:41:33 +0100

    add handle submit function

--- assets/js/searchEmployers.js
+++ assets/js/searchEmployers.js
@@ -1,106 +1,11 @@
-const jobs = {
-  categories: [
-    {
-      All: "All",
-      result: 25000,
-    },
-    {
-      emploie: "Emplois",
-      result: 10000,
-    },
-    {
-      Entreprise: "Entrepries",
-      result: 3000,
-    },
-    {
-      poste: "Poste",
-      result: 20,
-    },
-  ],
-  companies: [
-    {
-      Keyrus: 24000,
-    },
-    {
-      SAGEMCOM: 260,
-    },
-    {
-      goDev: 14000,
-    },
-  ],
-  Experience: ["Internship | alternant", "First Job"],
-  AnnualSalary: [15000, 10000, 5000, 3000],
-  jobs: [
-    {
-      position: "Software Engineer",
-      title: "Backend Developer",
-      description:
-        "You will help us build API for our compression infrastructure.",
-      url: "http://piedpiper.com/jobs/backend-developer",
-      type: "full-time",
-      posted: "2015-01-20",
-      location: "US",
-      skills: ["Python", "Javascript", "Redis", "React js", "HTML / CSS"],
-      salaryRange: {
-        from: 90000,
-        to: 100000,
-        currency: "USD",
-      },
-      equity: {
-        from: 0.005,
-        to: 0.01,
-      },
-      perks: ["free food", "gym membership"],
-      apply: "http://piedpiper.com/jobs/backend-developer/apply",
-      offers: 22,
-    },
-    {
-      position: "Software Engineer",
-      title: "Frontend Developer",
-      description:
-        "You will help us build dashboard for our compression infrastructure.",
-      url: "http://piedpiper.com/jobs/frontend-developer",
-      type: "full-time",
-      posted: "2015-01-20",
-      location: "US",
-      skills: ["Javascript", "Angular.js", "CSS"],
-      salaryRange: {
-        from: 90000,
-        to: 100000,
-        currency: "USD",
-      },
-      equity: {
-        from: 0.005,
-        to: 0.01,
-      },
-      offers: 150,
-    },
-    {
-      position: "UI/UX Designer",
-      title: "UX Designer",
-      type: "full-time",
-      posted: "2015-01-20",
-      location: "US",
-      skills: ["HTML", "CSS", "Photoshop"],
-      salaryRange: {
-        from: 90000,
-        to: 100000,
-        currency: "USD",
-      },
-      equity: {
-        from: 0.005,
-        to: 0.01,
-      },
-      perks: ["free food", "gym membership"],
-      apply: "http://piedpiper.com/jobs/ux-designer/apply",
-      offers: 1000,
-    },
-  ],
-};
+
 //variable declaration
 const selectBtn = document.querySelectorAll(".select-btn");
 items = document.querySelectorAll(".item");
 listDynamicItems = document.getElementById("listDynamicItems");
+let Skills = document.querySelectorAll('input[name="Skills"]:checked');
+
+  console.log(Skills)
 
 itemtext = document.querySelectorAll(".item-text");
 //********nav filter************** */
@@ -134,9 +39,15 @@ function submitHandler(event) {
 
   // get Inputs values
   let jobOffer = document.querySelector('input[name="jobOffer"]:checked').value;
-  let Skills = document.querySelector('input[name="Skills"]:checked').value;
+  let Skills = document.querySelector('input[name="Skills"]:checked');
   let jobType=document.querySelector('input[name="jobType"]:checked').value;
-  console.log(jobOffer, Skills);
+  skill = ['Iphone & Android','Java & Ajax']    
+      const btnspan = document.querySelector('#jobsOffers .btn-text')
+      btnspan.innerHTML=""
+      const span = document.createElement('span')
+      span.style=" font-size: 14px;background: #C4C4C4;width: max-content;border:1px solid #C4C4C4 ;border-radius: 14px;margin-left:6px"
+      span.innerHTML=jobOffer ;
+      btnspan.appendChild(span)
   // Build body request
   debugger;
 }
@@ -153,12 +64,9 @@ Skills = document.querySelectorAll("#Skills .item");
 
 Skills.forEach((item) => {
   item.addEventListener("click", (event) => {
-    console.log(item.classList.toggle("checked"));
-    item.classList.toggle("checked");
     let checked = document.querySelectorAll("#SkillsList .item .checked"),
       btnText = document.querySelector("#Skills .btn-text");
-
-
+ 
   });
 });
 //Telétravail filter
@@ -170,7 +78,7 @@ typeOfJob.forEach((item) => {
 
     let checked = document.querySelectorAll(".item .checked"),
       btnText = document.querySelector("#typeOfJob .btn-text");
-
+   
   
   });
 });
